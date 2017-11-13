a30dc5728fdeb9796cca550453c861c9e7b8a72b
GregoirePiat <gregoire.piat@outlook.fr>
2017-11-13 13:31:08 +0100

    Add basic call to food facts ai

--- init.py
+++ init.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import requests
+import json
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,3 +10,10 @@ def hello():
 @app.route("/coucou/<string:username>")
 def coucou(username):
     return "Coucou %s" % username
+
+@app.route("/yolo")
+def yolo():
+    response = requests.get('https://fr.openfoodfacts.org/api/v0/produit/009800892204.json')
+    print (type(response.json()))
+    # json.loads(response)
+    return response.json()['product']['packaging']
