beaac8e26bd24481af953a8404131c316b088bbc
GregoirePiat <gregoire.piat@outlook.fr>
2017-11-13 21:01:35 +0100

    Add first steps for flask login

--- README.md
+++ README.md
@@ -4,5 +4,10 @@
 
 ## How to run
 `pip install Flask`  
+`pip install flask-login`  
 `export FLASK_APP=init.py | flask run`  
 App is launched on http://localhost:5000/
+
+## Relevant docs
+https://pythonhosted.org/Flask-Security/  
+https://flask-login.readthedocs.io/en/latest/
--- init.py
+++ init.py
@@ -1,11 +1,18 @@
 from flask import Flask
 import requests
 import json
+import LoginManager
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    login_manager = LoginManager()
+    login_manager.init_app(app)
     return "Hello World!"
+
+@app.route("/coucou/<string:username>")
+def coucou(username):
+    return "Coucou %s" % username
 
 @app.route("/coucou/<string:username>")
 def coucou(username):
