a2892545513c3afdc0b4ee3e90ddfb7355b3c889
Thibault Dubois <dudub74@gmail.com>
2017-11-17 15:37:44 +0100

    refactored code, added get functions

--- init.py
+++ init.py
@@ -5,6 +5,8 @@ import re
 import json
 
 # import LoginManager
+
+
 app = Flask(__name__)
 app.before_request(bind_request_params)
 
@@ -21,39 +23,38 @@ def coucou(username):
     return "Coucou %s" % username
 
 
-
-@app.route("/yolo")
-def yolo():
-    response = requests.get('https://fr.openfoodfacts.org/api/v0/produit/009800892204.json')
-    print (type(response.json()))
-    # json.loads(response)
-    return response.json()['product']['packaging']
+@app.route("/test")
+def test():
+    return get_component_name('009800892204') + str(get_component_fat('009800892204'))
 
 
 # just return request.params
 # Example : curl -X POST http://localhost:5000/recipe/compute -d 'components[009800892204]=100g&components[009800892204]=10g'
 @app.route('/recipe/compute', methods=['GET'])
 def computeRecipe():
+    for component in request.args.getlist('component'):
+        fat_value = get_component_fat(component)
+        print(component)
+        print(fat_value)
 
-    componentCount =  len(request.args.getlist('component'))
-    for i in range(0, componentCount):
-        component = request.args.getlist('component')[i]
-        print (component)
-        print (getFatValue(component))
-    return 'yolo'
+    return (str(component))
 
 
-def getFatValue(bulkComponent):
-    barcode = ""
-    quantity = ""
-    splitComponent = bulkComponent.split('|');
-    barcode = splitComponent[0]
-    quantity = splitComponent[1]
-    print (barcode)
-    print (quantity)
 
-    quantity = 10
 
-    response = requests.get('https://fr.openfoodfacts.org/api/v0/produit/' + barcode + '.json')
-    fat100g = response.json()['product']['nutriments']['fat_100g']
+def get_component(component_id):
+    return requests.get('https://fr.openfoodfacts.org/api/v0/produit/' + str(component_id) + '.json').json()
+
+def get_component_name(component_id):
+    component = get_component(component_id)
+    return component['product']['product_name_en']
+
+def get_component_fat(component_id, quantity=100):
+    component = get_component(component_id)
+    fat100g = component['product']['nutriments']['fat_100g']
     return fat100g / 100 * quantity
+
+def get_recipe_fat(components_id):
+    total_fat = 0
+    for component_id in components_id:
+        total_fat += get_component_fat(component_id)
