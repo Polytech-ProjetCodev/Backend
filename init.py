d2066ca8da94d2cc825768ef40aaa6cae1e45d60
Thibault Dubois <dudub74@gmail.com>
2017-11-17 17:17:20 +0100

    updates postman config + working recipe/compute route for fat

--- init.py
+++ init.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template, jsonify
-from flask_request_params import bind_request_params
+# from flask_request_params import bind_request_params
 import requests
 import re
 import json
@@ -8,7 +8,7 @@ import json
 
 
 app = Flask(__name__)
-app.before_request(bind_request_params)
+# app.before_request(bind_request_params)
 
 
 @app.route("/")
@@ -28,33 +28,30 @@ def test():
     return get_component_name('009800892204') + str(get_component_fat('009800892204'))
 
 
-# just return request.params
-# Example : curl -X POST http://localhost:5000/recipe/compute -d 'components[009800892204]=100g&components[009800892204]=10g'
 @app.route('/recipe/compute', methods=['GET'])
 def computeRecipe():
+    """this route takes in parameter a bunch of components, with their quantity, and return the amount of fat it contains."""
+    total_fat = 0
     for component in request.args.getlist('component'):
-        fat_value = get_component_fat(component)
-        print(component)
-        print(fat_value)
-
-    return (str(component))
-
-
+        component = component.split('|')
+        component_id, quantity = component[0], component[1]
+        total_fat += get_component_fat(component_id, float(quantity))
+    return str(total_fat)
 
 
 def get_component(component_id):
+    """returns a dictionnary containing all information provided by the openfoodfacts.org API from the ID passed in parameter"""
     return requests.get('https://fr.openfoodfacts.org/api/v0/produit/' + str(component_id) + '.json').json()
 
+
 def get_component_name(component_id):
+    """returns the name of the product of which the ID was passed in parameter"""
     component = get_component(component_id)
     return component['product']['product_name_en']
 
+
 def get_component_fat(component_id, quantity=100):
+    """returns the amount of fat by 100g of the product of which the ID was passed in parameter"""
     component = get_component(component_id)
     fat100g = component['product']['nutriments']['fat_100g']
     return fat100g / 100 * quantity
-
-def get_recipe_fat(components_id):
-    total_fat = 0
-    for component_id in components_id:
-        total_fat += get_component_fat(component_id)
