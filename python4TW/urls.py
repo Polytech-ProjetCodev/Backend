0edf95cc2f17688ae08cd8d60847931e092f35df
ThibaultDub <dudub74@gmail.com>
2018-01-13 14:56:34 +0100

    added route for single ingredient with its barcode

--- python4TW/urls.py
+++ python4TW/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     # url(r'^ingredient$', views.IngredientList, name='index'),
     path('', views.welcome),
     path('ingredient/', views.IngredientList.as_view()),
+    path('ingredient/<int:pk>/', views.IngredientDetail.as_view())
     # path('component/', views.ComponentList)
     # url(r'^test/(?P<barcode>[0-9]{12})$', views.test, name="test"),
     # url(r'^testinsert/(?P<barcode>[0-9]{12})$', views.testinsert, name="testinsert")
