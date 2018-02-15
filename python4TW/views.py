04f87767817ad6e9f30c5eaae17a91302c7f7703
Grégoire Piat <gregoire.piat@etu.univ-lyon1.fr>
2018-02-15 15:43:55 +0100

    Prepare merge

--- python4TW/views.py
+++ python4TW/views.py
@@ -173,5 +173,6 @@ class UserDetail(generics.RetrieveAPIView):
 
 
 class UserCreate(generics.CreateAPIView):
+    permission_classes=[permissions.AllowAny]
     queryset = User.objects.all()
     serializer_class = UserSerializer
